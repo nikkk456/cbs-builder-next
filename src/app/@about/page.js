2a5320f3d38028b11cc503c1434856631d55cdb3
Nikhil Mishra <104861212+nikkk456@users.noreply.github.com>
2024-03-22 01:42:33 +0530

    Created Next Js version of CBS Builder

---
+++ src/app/@about/page.js
@@ -0,0 +1,115 @@
+"use client"
+import { useEffect, useState } from "react"
+import Modal from 'bootstrap/js/dist/modal';
+
+export default function About() {
+    const [loader, setLoader] = useState(false);
+    const [input, setinput] = useState({});
+    var myModal;
+    const formvalue = (e) => {
+        setinput({ ...input, [e.target.name]: e.target.value });
+    }
+    const submitform = (event) => {
+        event.preventDefault();
+        const data_to = { name: input.name, email: input.email, phone: input.phone }
+        console.log(data_to.name);
+        if (input.name === undefined || input.phone === undefined || input.email === undefined) {
+            alert("Kindly fill all the details");
+        } else {
+            setLoader(true);
+            axios.post("http://localhost:0080/cbsbuilderapi/contact_popup.php", data_to).then((response) => {
+                alert(response.data.result);
+                setLoader(false);
+                window.open('https://drive.google.com/file/d/1HJdtWyO19_2PeV17iMyWETKS3iYw_I8M/view?usp=drive_link', '_blank');
+
+            });
+
+        }
+    }
+    useEffect(() => {
+        const myModalElement = document.getElementById('exampleModal');
+        myModal = new Modal(myModalElement);
+    }, [])
+    const handleClick = () => {
+        myModal.show();
+    }
+    const handleClose=()=>{
+        console.log("close this modal");
+        myModal.hide();
+    }
+    return (
+        <div className='container' id="About">
+            <div className='row'>
+                <h2 className='text-center'><span style={{ width: "10%", borderBottom: "2px double #3a5c5c" }}>About Us</span></h2>
+            </div>
+            <div className='row'>
+                <div className='col-md-5'><img src='./Aboutus.png' alt='cbs_building' style={{ width: "100%" }}></img></div>
+                <div className='col-md-7 '>
+                    <div className='container'>
+                        <div className='row'><p>
+                            At CBS, a family-run business with over 50 years of real estate history, the new generation is at the helm, bringing fresh energy and ideas to luxury builder floors in Gurgaon. Our goal is simple: to redefine luxury living. With deep roots in the industry and guided by values like dedication, honesty, and simplicity, we're not just building homes; we're crafting lifestyles. Our approach is straightforward - we aim to deliver unparalleled luxury in every floor we construct, ensuring our customers receive not just a home, but a space that holds lasting value. CBS stands out by focusing on what matters most: creating luxury living spaces that you'll be proud to call home.
+                        </p>
+                        </div>
+                        <div className='row'>
+                            <div className='col-md-5 col-6'>
+                                <button className='buttoncbs' onClick={handleClick}>Download Brochure</button>
+                            </div>
+                            <div className='col-md-5 col-5'>
+                                <button className='buttoncbs' onClick={handleClick} style={{width:"100px"}}>Call Now!</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header" style={{ backgroundColor: "#3a5c5c", color: "white" }}>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Get Best Offer</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            {
+                                loader ? <div className='d-flex justify-content-center align-items-center'>
+                                    <div class="spinner-grow" role="status">
+                                        <span class="visually-hidden">Loading...</span>
+                                    </div>
+                                </div> : <form >
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputPassword1" className="form-label">Your Full Name</label>
+                                        <input type="text" onChange={formvalue} className="form-control" id='name' name="name" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                                        <input type="email" onChange={formvalue} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" />
+                                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="exampleInputPassword1" className="form-label" >Your Phone Number</label>
+                                        <input type="number" onChange={formvalue} className="form-control" id="exampleInputPassword1" name="phone" />
+                                    </div>
+
+                                </form>
+                            }
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="buttoncbs" style={{ backgroundColor: "red", width: "70px" }} onClick={handleClose}>Close</button>
+                            <button type="submit" className="buttoncbs" onClick={submitform} style={{ width: "200px" }}>Download Brochure</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+        <div className="container-fluid">
+          <Link className="navbar-brand" href="/" style={{ width: '10%' }}>
+            <img src='./logo.png' alt='logo' style={{ width: '100%' }} />
+          </Link>
+          <button className="navbar-toggler" type="button" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#About">About Us</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#Gallery">Gallery</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#Amenities">Amenities</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#Pricelist">Price</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#Floorplan">Floorplan</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#Location">Location Advantages</a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
